debf40c83b51c561d36f83a228653e0c538f1822
VinicioJesus <viniciojesus298@gmail.com>
2022-01-26 16:12:23 -0300

    validando e separando mais coisas

--- js/main.js
+++ js/main.js
@@ -1,36 +1,47 @@
 "use strict"; /* navegador, siga as regras e mostre mais erros da minha programação*/
 
-function camposvalidos(nome, altura, peso) {
-  return nome !== "" && altura !== "" && peso !== "";
+function camposValidos(nome, altura, peso) {
+  // return nome !== "" && altura !== "" && peso !== "";
+  return document.getElementById("formulario").reportValidity();
 }
 
-function mostrarResultado() {
+function calcularImc(altura, peso) {
+  return peso / (altura * altura);
+}
+function classificarImc(imc) {
+  let texto;
+  if (imc < 18.5) {
+    texto = "e você está abaixo do peso";
+  } else if (imc < 25) {
+    texto = "e você está com peso ideal. Parabéns";
+  } else if (imc < 30) {
+    texto = "e você está levemente acima do peso.";
+  } else if (imc < 35) {
+    texto = "e você está com obesidade grau I";
+  } else if (imc < 40) {
+    texto = "e você está com obesidade grau II";
+  } else {
+    texto = "e você está com obesidade grau III";
+  }
+  return texto;
+}
+
+function mostrarResultados() {
   const nome = document.getElementById("nome").value;
   const altura = document.getElementById("altura").value;
   const peso = document.getElementById("peso").value;
   const resultado = document.getElementById("resultado");
 
-  if (camposvalidos(nome, altura, peso)) {
-    const imc = peso / (altura * altura);
+  if (camposValidos(altura, peso)) {
+    const imc = calcularImc(altura, peso);
+    let texto = classificarImc(imc);
 
-    let texto;
-    if (imc < 18.5) {
-      texto = "e você está abaixo do peso";
-    } else if (imc < 25) {
-      texto = "e você está com peso ideal. Parabéns";
-    } else if (imc < 30) {
-      texto = "e você está levemente acima do peso.";
-    } else if (imc < 35) {
-      texto = "e você está com obesidade grau I";
-    } else if (imc < 40) {
-      texto = "e você está com obesidade grau II";
-    } else {
-      texto = "e você está com obesidade grau III";
-    }
     resultado.textContent = `${nome} seu imc é  ${imc.toFixed(2)} ${texto}`;
   } else {
     resultado.textContent = "preencha todos os campos!";
   }
 }
 
-document.getElementById("calcular").addEventListener("click", mostrarResultado);
+document
+  .getElementById("calcular")
+  .addEventListener("click", mostrarResultados);
